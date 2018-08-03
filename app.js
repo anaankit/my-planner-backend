2e62c28dc2ab5a98c3c1d94302a06936d20c2ca4
unknown <anaankit992@gmail.com>
2018-08-04 03:09:40 +0530

    three

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const bodyparser =  require('body-parser');
 const fs = require('fs');
 const app = express();
 const http = require('http');
-const appConfig =  require('./config/appconfig');
+const appConfig =  {
+port = 3000,
+allowedCorsOrigin = "*",
+env = "dev",
+db = {
+    uri : "mongodb://127.0.0.1:27017/tea1"
+}
+}
+
 const logger = require('./app/libs/loggerLib');
 const routeLoggerMiddleware = require('./app/middlewares/routeLogger.js');
 const globalErrorMiddleware = require('./app/middlewares/appErrorHandler');
